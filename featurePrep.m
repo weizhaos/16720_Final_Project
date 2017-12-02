f8d93897a9a461bb22be725c0184e401f54780de
Geng Yan <gengyan@Gengs-MacBook-Pro.local>
2017-12-02 17:49:11 -0500

    bugfix

--- featurePrep.m
+++ featurePrep.m
@@ -8,31 +8,33 @@ function [featurePrev, featureCurrent, k] = featurePrep(featurePrev, k, flowmap,
 % features with know depth, and then features with unknown depth
 imgsize = size(flowmap); % [height, width, 2]
 nFeature = size(featurePrev,1);
-for i = 1:k
+i = 1;
+while(i<=k)
     pos = featurePrev(i,1:2);
     flow = flowmap(floor(pos(1)),floor(pos(2)),:);
     new_pos = floor(pos + [flow(2) flow(1)]);
     if new_pos(1) <= 0 || new_pos(1) > imgsize(1) || new_pos(2) <= 0 || new_pos(2) > imgsize(2)
         % remove the feature
         featurePrev(i,:) = [];
-        i = i - 1;
         k = k - 1;
         nFeature = nFeature - 1;
     else
         featureCurrent(i,:) = [new_pos(1) new_pos(2) depCurr(new_pos(1),new_pos(2))];
+        i = i+1;
     end
 end
 % without depth
-for i = k+1:nFeature
+i = k+1;
+while(i<=nFeature)
     pos = featurePrev(i,1:2);
     flow = flowmap(floor(pos(1)),floor(pos(2)),:);
     new_pos = floor(pos + [flow(2) flow(1)]);
     if new_pos(1) <= 0 || new_pos(1) > imgsize(1) || new_pos(2) <= 0 || new_pos(2) > imgsize(2)
         % remove the feature
         featurePrev(i,:) = [];
-        i = i - 1;
         nFeature = nFeature - 1;
     else
+        i = i+1;
         featureCurrent(i,:) = [new_pos(1) new_pos(2) 1];
     end
 end
