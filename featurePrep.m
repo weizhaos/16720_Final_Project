f9c0f8b98fd718ccb7eef07e6b4a4cd4271cbc38
Geng Yan <gengyan@Gengs-MacBook-Pro.local>
2017-12-02 19:15:26 -0500

    bugfix

--- featurePrep.m
+++ featurePrep.m
@@ -10,6 +10,7 @@ imgsize = size(flowmap); % [height, width, 2]
 nFeature = size(featurePrev,1);
 i = 1;
 while(i<=k)
+    i
     pos = featurePrev(i,1:2);
     flow = flowmap(floor(pos(1)),floor(pos(2)),:);
     new_pos = floor(pos + [flow(2) flow(1)]);
@@ -19,12 +20,13 @@ while(i<=k)
         k = k - 1;
         nFeature = nFeature - 1;
     else
+        i
+        featureCurrent(i,:)
         featureCurrent(i,:) = [new_pos(1) new_pos(2) depCurr(new_pos(1),new_pos(2))];
         i = i+1;
     end
 end
 % without depth
-i = k+1;
 while(i<=nFeature)
     pos = featurePrev(i,1:2);
     flow = flowmap(floor(pos(1)),floor(pos(2)),:);
@@ -34,8 +36,8 @@ while(i<=nFeature)
         featurePrev(i,:) = [];
         nFeature = nFeature - 1;
     else
-        i = i+1;
         featureCurrent(i,:) = [new_pos(1) new_pos(2) 1];
+        i = i+1;
     end
 end
 
